309b6fe2dd15bf6a8713f7500732fcad03d186d8
Arun Kumar Verma <arunkumarverma22022000@gmail.com>
2021-08-16 00:34:53 +0530

    Deleted .exe files and add C++ extentions tasks for VS CODE

--- 755 - 487--3279.cpp
+++ 755 - 487--3279.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include<string.h>
+#include <string.h>
 using namespace std;
 struct dat
 {
@@ -7,116 +7,128 @@ struct dat
 	char ch;
 };
 
-
 int main()
 {
-	
-	int q,n,count;
-	string s1,s2;
-	cin>>q;
-		cin.ignore();
-	while(q)
+
+	int q, n, count;
+	string s1, s2;
+	cin >> q;
+	cin.ignore();
+	while (q)
 	{
 		cin.ignore();
-		cin>>n;
+		cin >> n;
 		cin.ignore();
-		map<string,int> numb;
-		map<string,int>::iterator it1;
-		
-		for(int l=0;l<n;l++)
+		map<string, int> numb;
+		map<string, int>::iterator it1;
+
+		for (int l = 0; l < n; l++)
 		{
-		s2="";
-		getline(cin,s1);
-		stringstream s(s1);
-		count=0;
-		char ch;
-		int m=s1.length();
-		for(int i=0;i<m;i++)
-		{
-			s>>ch;
-			if(ch=='-') continue;
-			count++;
-			//if(count==8 ) break;
-			if(count==4) s2=s2+'-';
-			switch(ch)
+			s2 = "";
+			getline(cin, s1);
+			stringstream s(s1);
+			count = 0;
+			char ch;
+			int m = s1.length();
+			for (int i = 0; i < m; i++)
 			{
-				
-				case '0': ch='0';
-				break;
-				case '1': ch='1';
-				break;
+				s >> ch;
+				if (ch == '-')
+					continue;
+				count++;
+				//if(count==8 ) break;
+				if (count == 4)
+					s2 = s2 + '-';
+				switch (ch)
+				{
+
+				case '0':
+					ch = '0';
+					break;
+				case '1':
+					ch = '1';
+					break;
 				case '2':
 				case 'A':
 				case 'B':
-				case 'C': ch='2';
+				case 'C':
+					ch = '2';
 					break;
 				case '3':
 				case 'D':
 				case 'E':
-				case 'F': ch='3';
+				case 'F':
+					ch = '3';
 					break;
 				case '4':
 				case 'G':
 				case 'H':
-				case 'I': ch='4';
+				case 'I':
+					ch = '4';
 					break;
 				case '5':
 				case 'J':
 				case 'K':
-				case 'L': ch='5';
+				case 'L':
+					ch = '5';
 					break;
 				case '6':
 				case 'M':
 				case 'N':
-				case 'O': ch='6';
+				case 'O':
+					ch = '6';
 					break;
 				case '7':
 				case 'P':
 				case 'R':
-				case 'S': ch='7';
+				case 'S':
+					ch = '7';
 					break;
 				case '8':
 				case 'T':
 				case 'U':
-				case 'V': ch='8';
+				case 'V':
+					ch = '8';
 					break;
 				case '9':
 				case 'W':
 				case 'X':
-				case 'Y': ch='9';
+				case 'Y':
+					ch = '9';
 					break;
-					default: ch=ch;
-					
+				default:
+					ch = ch;
+				}
+
+				s2 = s2 + ch;
 			}
-			
-			s2=s2+ch;
-		}
-		it1=numb.find(s2);
-	//	cout<<s1<<"\t"<<s2<<endl;
-		if(it1==numb.end()) numb[s2]=1;
-		else{
-		    int a=numb[s2];
-		    numb[s2]=a+1;
-		}
-		
-		}
-		count=0;
-		for( it1=numb.begin();it1!=numb.end();it1++)
-		{
-			if(it1->second>1) 
+			it1 = numb.find(s2);
+			//	cout<<s1<<"\t"<<s2<<endl;
+			if (it1 == numb.end())
+				numb[s2] = 1;
+			else
 			{
-			    cout<<it1->first<<" "<<it1->second<<endl;
-			    count=1;
+				int a = numb[s2];
+				numb[s2] = a + 1;
+			}
+		}
+		count = 0;
+		for (it1 = numb.begin(); it1 != numb.end(); it1++)
+		{
+			if (it1->second > 1)
+			{
+				cout << it1->first << " " << it1->second << endl;
+				count = 1;
 			}
 		}
 		numb.clear();
-		if(!count) cout<<"No Duplicates"<<endl;
-		
-		q--;
-		if(q) cout<<endl;
-	}
-	
-	
-    return 0;
-}
+		if (!count)
+			cout << "No Duplicates" << endl;
 
+		q--;
+		if (q)
+			cout << endl;
+	}
+
+	return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vll vector<ll>
+using namespace std;
+int main()
+{
+    string s1 = "Hello Joke";
+    cout << s1;
+    return 0;
+}
