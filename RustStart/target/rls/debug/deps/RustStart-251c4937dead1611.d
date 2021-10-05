156c7b802a4d28fff7b70244772bd7ed6d3d5403
Arun Kumar Verma <44711133+akverma00@users.noreply.github.com>
2021-10-05 15:47:27 +0530

    Revert "Added Rust Project"

--- RustStart/target/rls/debug/deps/RustStart-251c4937dead1611.d
+++
@@ -1,5 +0,0 @@
-c:\xampp\htdocs\DSA\RustStart\target\rls\debug\deps\RustStart-251c4937dead1611.rmeta: src\main.rs
-
-c:\xampp\htdocs\DSA\RustStart\target\rls\debug\deps\RustStart-251c4937dead1611.d: src\main.rs
-
-src\main.rs:
--- RustStart/target/rls/debug/deps/RustStart-d1326318c594bcfc.d
+++
@@ -1,5 +0,0 @@
-c:\xampp\htdocs\DSA\RustStart\target\rls\debug\deps\RustStart-d1326318c594bcfc.rmeta: src\main.rs
-
-c:\xampp\htdocs\DSA\RustStart\target\rls\debug\deps\RustStart-d1326318c594bcfc.d: src\main.rs
-
-src\main.rs:
